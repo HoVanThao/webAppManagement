ff426358c6fd27954fbd29b78b2d7af68564f251
HoVanThao <hovanthao0611cs@gmail.com>
2024-06-24 23:29:57 +0700

    04 - Landing Page

--- my-app-thao/src/App.jsx
+++ my-app-thao/src/App.jsx
@@ -1,4 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomeLayout, Landing, Register, Login, DashboardLayout, Error } from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ]
+  },
+]);
+
 const App = () => {
-  return <h1>hedddddrrllo</h1>
+  return <RouterProvider router={router} />
 };
 export default App; 
